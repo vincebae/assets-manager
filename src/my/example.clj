bae002056d7ecde0775dd8399b8fbbaa4dbe31f0
Seung-Bin Bae <vincebae@gmail.com>
2025-05-31 22:18:56 -0700

    Some more example code to use Vis UI

--- src/my/example.clj
+++ src/my/example.clj
@@ -1,42 +1,74 @@
 (ns my.example
   (:import
    [com.badlogic.gdx ApplicationAdapter Gdx]
-   [com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application Lwjgl3ApplicationConfiguration]
+   [com.badlogic.gdx.backends.lwjgl3
+    Lwjgl3Application
+    Lwjgl3ApplicationConfiguration]
    [com.badlogic.gdx.graphics GL20]
    [com.badlogic.gdx.scenes.scene2d Stage]
    [com.badlogic.gdx.scenes.scene2d.ui Skin Label TextButton Table]
    [com.badlogic.gdx.scenes.scene2d.utils ChangeListener]
-   [com.badlogic.gdx.utils.viewport ScreenViewport]))
+   [com.badlogic.gdx.utils.viewport ScreenViewport]
+   [com.kotcrab.vis.ui VisUI]
+   [com.kotcrab.vis.ui.widget VisLabel VisTextButton]
+   [com.kotcrab.vis.ui.widget.file
+    FileChooser
+    FileChooserListener
+    FileChooser$Mode]))
 
 (def ^:private ^:const skin-dir "assets/default/")
 
+(defn show-file-dialog
+  [stage]
+  (let [chooser (FileChooser. FileChooser$Mode/OPEN)]
+    (.setSize chooser 400 300)
+    (.setListener chooser
+                  (reify FileChooserListener
+                    (selected [_ files]
+                      (doseq [f files]
+                        (println "Selected:" (.file f))))
+                    (canceled [_]
+                      (println "Selection canceled."))))
+    (.addActor @stage chooser)))
+
 (defn- create-ui
   [stage]
+  (VisUI/load)
+
   (let [skin (Skin. (.local Gdx/files (str skin-dir "uiskin.json")))
         table (Table.)
-        initial-label (Label. "Hello from Clojure!" skin)
-        btn (TextButton. "Click Me" skin)]
+        normal-label (Label. "Hello from Clojure!" skin)
+        normal-button (TextButton. "Click Me" skin)
+        vis-label (VisLabel. "Vis Label")
+        vis-button (VisTextButton. "Open File Chooser")]
+
      ;; Layout setup
     (.setFillParent table true)
-    
+
     ;; Add initial label
     (doto table
-      (.add initial-label)
+      (.add normal-label)
+      (.row)
+      (.add normal-button)
+      (.row)
+      (.add vis-label)
+      (.row)
+      (.add vis-button)
       (.row))
-    
-    ;; Add button and listener
-    (doto table
-     (.add btn)
-     (.row))
-    
-    (.addListener btn
-     (proxy [ChangeListener] []
-      (changed [event actor]
-       (let [new-label (Label. "Dynamically added label!" skin)]
-        (doto table
-         (.add new-label)
-         (.row)
-         (.pack))))))
+
+    (.addListener vis-button
+                  (proxy [ChangeListener] []
+                    (changed [event actor]
+                      (show-file-dialog stage))))
+
+    (.addListener normal-button
+                  (proxy [ChangeListener] []
+                    (changed [event actor]
+                      (let [new-label (Label. "Dynamically added label!" skin)]
+                           (doto table
+                             (.add new-label)
+                             (.row)
+                             (.pack))))))
 
     ;; Attach to stage
     (.addActor @stage table)))
@@ -61,20 +93,21 @@
       (.draw @stage))
 
     (dispose []
-     (.dispose @stage))))
+      (VisUI/dispose)
+      (.dispose @stage))))
 
 (defn- create-config
   [config]
   (let [app-config (Lwjgl3ApplicationConfiguration.)
         {:keys [title w h]} config]
     (doto app-config
-     (.setTitle title)
-     (.setWindowedMode w h))
+      (.setTitle title)
+      (.setWindowedMode w h))
     app-config))
 
 (defn init
   []
-  {:config {:title "Clojure LibGDX Scene2D" :w 600 :h 400}
+  {:config {:title "Clojure LibGDX Scene2D" :w 800 :h 600}
    :state (atom {})
    :stage (atom nil)})
 
